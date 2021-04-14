a629cb0dba14b6b67f02e2bc0161e1a8ca1487fc
Kentucky1988 <lulchukdima@gmail.com>
2021-04-15 01:35:42 +0300

    add MappingProfile

---
+++ RebitMqPostman/Infrastructure/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RebitMqPostman.BLL.Models;
+using RebitMqPostman.Models.v1;
+
+namespace RebitMqPostman.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateCustomerModel, Customer>().ForMember(x => x.Id, opt => opt.Ignore());
+        }
+    }
+}
